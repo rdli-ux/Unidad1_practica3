711029f96583e2cd625a8f6bb190dfad549fe50f
rdli-ux <1225100759.rdli@gmail.com>
2026-09-18 19:44:46 -0600

    Cambios

---
+++ main.py
@@ -0,0 +1,11 @@
+def menu():
+    print("1. Agregar producto")
+    print("2. Eliminar producto")
+    print("3. Actualizar producto")
+    print("4. Mostrar inventario")
+    print("5. Salir")
+
+if __name__ == "__main__":
+    print("Inventario de mercancía de tienda de abarrotes.")
+    menu()
+
